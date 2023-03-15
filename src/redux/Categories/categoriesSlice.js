4c0dceaaee37be7c8a99cb49b04945d4b6b1e24a
Odongo006 <www.henryodongo006@gmail.com>
2023-03-15 14:33:21 +0300

    Add reducers and actions

---
+++ src/redux/Categories/categoriesSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  categories: [],
+};
+
+export const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {
+    checkStatus: (state, action) => {
+      const newState = {
+        ...state,
+        categories: action.payload === 'Under construction'
+          ? 'Under construction'
+          : state.categories,
+      };
+
+      // return the new state object
+      return newState;
+    },
+  },
+});
+
+export const { checkStatus } = categoriesSlice.actions;
+
+export default categoriesSlice.reducer;
